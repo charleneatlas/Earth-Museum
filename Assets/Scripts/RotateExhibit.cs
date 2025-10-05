73f893355afb8d63f14adcf3943bd49835d49f56
Charlene Atlas <drummershoujo@gmail.com>
2025-10-05 03:20:16 -0700

    Added phytoplankton models and NASA CO2 data, and Earth rotation fix

--- Assets/Scripts/RotateExhibit.cs
+++ Assets/Scripts/RotateExhibit.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RotateExhibit : MonoBehaviour
 {
-    public float rotationSpeed = 5;
+    public float rotationSpeed = -5;
     // Start is called before the first frame update
     void Start()
     {
